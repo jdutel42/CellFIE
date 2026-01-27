c8c508f66c621caae056b7cb5f48ba17a0f003c4
Jordan Dutel <jo.dutel@gmail.com>
2026-01-27 11:57:21 +0100

    Add navTab layout to app

--- ui.R
+++ ui.R
@@ -18,6 +18,7 @@ library(shinyFeedback)
 library(shinyjs)
 # BiocManager::install("qs")
 library(qs2)
+library(bslib)
 
 
 ###########
@@ -30,139 +31,171 @@ useShinyjs()
 ###################################################################################"
 # ---- User Interface ---- #
 
-ui <- fluidPage(
+ui <- page_fillable(
   
   ###################
   # ---- Title ---- #
   ###################
   
-  titlePanel("CellFIE :Cells & Features Interactive Explorer"),
+  titlePanel("CellFIE : Cells & Features Interactive Explorer"),
   
-  #####################
-  # ---- Sidebar ---- #
-  #####################
   
-  sidebarLayout(
-    sidebarPanel(
-      
-      ########################
-      # ---- Input file ---- #
-      ########################
-      
-      fileInput(
-        "sce_rds",
-        "1. Upload SingleCellExperiment RDS file",
-        accept = c(".rds", ".RDS")
-      ),
-      
-      # uiOutput("qs_ui"),
-      
-      ###################
-      # ---- Assay ---- #
-      ###################
-      
-      uiOutput("assay_ui"),
-      
-      #######################
-      # ---- Embedding ---- #
-      #######################
-      
-      uiOutput("embedding_ui"),
-      
-      # Change this part to dynamically get the available reductions from the SCE object !!!!!
-      
-      # selectInput(
-      #   "reduction",
-      #   "3. Which embedding ?",
-      #   # choices = c("UMAP_uwot", "TSNE", "PCA"),
-      #   choices = c("PCA", "TSNE", "UMAP"),
-      #   selected = "UMAP"
-      # ),
-      
-      #######################
-      # ---- Features ---- #
-      #######################
-      
-      # Change this part to dynamically get the available reductions from the SCE object !!!!!
-      
-      uiOutput("feature_ui"),
-      
-      ###################
-      # ---- Genes ---- #
-      ###################
-      
-      shinycssloaders::withSpinner(
-        uiOutput("gene_ui")
-      ),
-      
-      
-      
-      #########################
-      # ---- Plot Tuning ---- #
-      #########################
-      
-      sliderInput(
-        "pt_size",
-        "Point size",
-        min = 0.1,
-        max = 2,
-        value = 0.6,
-        step = 0.1
-      ),
-      
-      
-      ##################################
-      # ---- Plot Tuning Metadata ---- #
-      ##################################
-      
-      # shinycssloaders::withSpinner(
-      #   uiOutput("metadata_ui")
-      # ),
-      # 
-      # shinycssloaders::withSpinner(
-      #   uiOutput("metadata_val_ui")
-      # ),
+  ####################
+  # ---- NavTab ---- #
+  ####################
+  
+  navset_card_tab( 
     
-      
-      
-      
-      # selectInput(
-      #   "metadata",
-      #   "Choose a metadata to display",
-      #   nested_list_metadata
-      # ),
-      
-      
-      ###########################
-      # ---- Download Plot ---- #
-      ###########################
-      
-      # Button to download the PCA plot
-      downloadButton(
-        "download_plot_png", 
-        "Download PNG"
-      ),
-      
-      downloadButton(
-        "download_plot_pdf", 
-        "Download PDF"
-      )
-      
-    ),
+    nav_panel("FeaturePlot", 
+    #############################
+    # ---- FeaturePlot Tab ---- #
+    #############################
     
-    ################################
-    # ---- Plot visualization ---- #
-    ################################
-    
-    mainPanel(
-      shinycssloaders::withSpinner(
-        plotOutput(
-          "featureplot",
-          height = "750px"
+      #####################
+      # ---- Sidebar ---- #
+      #####################
+      
+      sidebarLayout(
+        sidebarPanel(
+          
+          ########################
+          # ---- Input file ---- #
+          ########################
+          
+          fileInput(
+            "sce_rds",
+            "1. Upload SingleCellExperiment RDS file",
+            accept = c(".rds", ".RDS")
+          ),
+          
+          # uiOutput("qs_ui"),
+          
+          ###################
+          # ---- Assay ---- #
+          ###################
+          
+          uiOutput("assay_ui"),
+          
+          #######################
+          # ---- Embedding ---- #
+          #######################
+          
+          uiOutput("embedding_ui"),
+          
+          # Change this part to dynamically get the available reductions from the SCE object !!!!!
+          
+          # selectInput(
+          #   "reduction",
+          #   "3. Which embedding ?",
+          #   # choices = c("UMAP_uwot", "TSNE", "PCA"),
+          #   choices = c("PCA", "TSNE", "UMAP"),
+          #   selected = "UMAP"
+          # ),
+          
+          #######################
+          # ---- Features ---- #
+          #######################
+          
+          # Change this part to dynamically get the available reductions from the SCE object !!!!!
+          
+          uiOutput("feature_ui"),
+          
+          ###################
+          # ---- Genes ---- #
+          ###################
+          
+          shinycssloaders::withSpinner(
+            uiOutput("gene_ui")
+          ),
+          
+          
+          
+          #########################
+          # ---- Plot Tuning ---- #
+          #########################
+          
+          sliderInput(
+            "pt_size",
+            "Point size",
+            min = 0.1,
+            max = 2,
+            value = 0.6,
+            step = 0.1
+          ),
+          
+          
+          ##################################
+          # ---- Plot Tuning Metadata ---- #
+          ##################################
+          
+          # shinycssloaders::withSpinner(
+          #   uiOutput("metadata_ui")
+          # ),
+          # 
+          # shinycssloaders::withSpinner(
+          #   uiOutput("metadata_val_ui")
+          # ),
+          
+          
+          
+          
+          # selectInput(
+          #   "metadata",
+          #   "Choose a metadata to display",
+          #   nested_list_metadata
+          # ),
+          
+          
+          ###########################
+          # ---- Download Plot ---- #
+          ###########################
+          
+          # Button to download the PCA plot
+          downloadButton(
+            "download_plot_png", 
+            "Download PNG"
+          ),
+          
+          downloadButton(
+            "download_plot_pdf", 
+            "Download PDF"
+          )
+          
+        ),
+        
+        ################################
+        # ---- Plot visualization ---- #
+        ################################
+        
+        mainPanel(
+          shinycssloaders::withSpinner(
+            plotOutput(
+              "featureplot",
+              height = "750px"
+            )
+          )
         )
       )
+    ),
+    
+    nav_panel("VolcanoPlot",
+              "Work in progress..."
+    ),
+    
+    nav_panel("About",
+              ################
+              # ---- About ---- #
+              ################
+              
+              "Some information about the application.",
+              # includeMarkdown("www/About.md")
     )
-  )
+              
+  ),
+  
+
+  
+
 )
 
 
